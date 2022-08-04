9a98d94ac2f94daceec758ee989b7b721f365f7b
panoptisDev <panoptis.network@gmail.com>
2022-08-04 04:18:10 +0200

    1commit

---
+++ pages/exchange.js
@@ -0,0 +1,107 @@
+import { useAddress } from "@thirdweb-dev/react";
+import Head from "next/head";
+import { useRouter } from "next/router";
+import React, { useEffect, useState } from "react";
+import Web3 from "web3";
+import { tokenABI, vendorABI } from "../contracts";
+const web3 = new Web3(Web3.givenProvider);
+
+function Exchange() {
+  const [tokens, setTokens] = useState();
+  const [matic, setMatic] = useState(0);
+  const address = useAddress();
+  const router = useRouter();
+
+  const purchase = async () => {
+    try {
+      const accounts = await web3.eth.getAccounts();
+      const vendor = new web3.eth.Contract(
+        vendorABI,
+        process.env.NEXT_PUBLIC_VENDOR_CONTRACT_ADDRESS
+      );
+      const request = await vendor.methods.buyTokens().send({
+        from: accounts[0],
+        value: web3.utils.toWei(matic.toString(), "ether"),
+      });
+      alert("You have successfully purchased JELLY tokens!");
+      console.log(request);
+    } catch (err) {
+      console.error(err);
+      alert("Error purchasing tokens");
+    }
+  };
+
+  const sell = async () => {
+    try {
+      const accounts = await web3.eth.getAccounts();
+      const tokenContract = new web3.eth.Contract(
+        tokenABI,
+        process.env.NEXT_PUBLIC_TOKEN_CONTRACT_ADDRESS
+      );
+      // Approve the contract to spend the tokens
+      let request = await tokenContract.methods
+        .approve(
+          process.env.NEXT_PUBLIC_VENDOR_CONTRACT_ADDRESS,
+          web3.utils.toWei(tokens, "ether")
+        )
+        .send({
+          from: accounts[0],
+        });
+
+      // Trigger the selling of tokens
+      const vendor = new web3.eth.Contract(
+        vendorABI,
+        process.env.NEXT_PUBLIC_VENDOR_CONTRACT_ADDRESS
+      );
+      request = await vendor.methods
+        .sellTokens(web3.utils.toWei(tokens, "ether"))
+        .send({
+          from: accounts[0],
+        });
+
+      alert("You have successfully sold JELLY tokens!");
+      console.log(request);
+    } catch (err) {
+      console.error(err);
+      alert("Error selling tokens");
+    }
+  };
+
+  useEffect(() => {
+    if (!address) router.replace("/");
+  }, [address]);
+
+  useEffect(() => {
+    setMatic(tokens / 6);
+  }, [tokens]);
+
+  return (
+    <div>
+      <Head>
+        <title>Swap JELLY tokens</title>
+      </Head>
+      <div className="exchange__container">
+        
+        <h1>Purchase JELLY Tokens</h1>
+        <div>0x398e45A300294Fd2e091F3B02fc2CcaEe30db779</div>
+        
+        <input
+          type="number"
+          placeholder="Amount of tokens"
+          className="exchange__textBox"
+          value={tokens}
+          onChange={(e) => setTokens(e.target.value)}
+        />
+        <div>Equivalent to {matic} MATIC </div> 
+        <button className="exchange__button" onClick={purchase}>
+          Purchase
+        </button>
+        <button className="exchange__button" onClick={sell}>
+          Sell
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Exchange;
